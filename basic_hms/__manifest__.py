736bdce61737dcc001e41b91f7a358bc1ffc9da7
funcionalNetBuro <122516846+funcionalNetBuro@users.noreply.github.com>
2023-11-06 09:49:36 +0100

    Update __manifest__.py

--- basic_hms/__manifest__.py
+++ basic_hms/__manifest__.py
@@ -35,7 +35,7 @@
     
 """,
 
-    "depends": ["base", "sale_management", "stock", "account"],
+    "depends": ["base", "sale_management", "stock", "account", "contacts"],
     "data": [
         'security/hospital_groups.xml',
         'security/ir.model.access.csv',
