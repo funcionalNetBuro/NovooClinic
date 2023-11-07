a1c12fcab8216e54fa341e5461f45c4d2871fbb4
funcionalNetBuro <122516846+funcionalNetBuro@users.noreply.github.com>
2023-11-07 19:18:04 +0100

    Update medical_patient.py

--- basic_hms/model/medical_patient.py
+++ basic_hms/model/medical_patient.py
@@ -42,7 +42,7 @@ class medical_patient(models.Model):
   
     x_alergias = fields.Boolean(string="Alergias")
     x_medicacion = fields.Boolean(string="Toma medicación")
-    x_intervenciones = fields.Boolean(string="Intervenciones")
+    x_intervencinoes = fields.Boolean(string="Intervenciones")
     x_dni_paciente = fields.Char(related='patient_id.vat', string="DNI", readonly=True)
     patient_id = fields.Many2one('res.partner',domain=[('is_patient','=',True)],string="Patient", required= True)
     name = fields.Char(string='ID', readonly=True)
