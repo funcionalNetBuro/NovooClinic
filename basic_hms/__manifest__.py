88ba00c6f05db01b94645eca697392ceeb8086da
funcionalNetBuro <122516846+funcionalNetBuro@users.noreply.github.com>
2023-11-08 17:13:33 +0100

    Update __manifest__.py

--- basic_hms/__manifest__.py
+++ basic_hms/__manifest__.py
@@ -12,30 +12,11 @@
     "currency": 'EUR',    
     "summary":"Aplicación de sistema de gestión clínica",
     "category": "Industries",
+    
     "description": """
-    BrowseInfo developed a new odoo/OpenERP module apps
-    This module is used to manage Hospital and Healthcare Management and Clinic Management apps. 
-    manage clinic manage Patient hospital in odoo manage Healthcare system Patient Management, 
-    Odoo Hospital Management odoo Healthcare Management Odoo Clinic Management
-    Odoo hospital Patients
-    Odoo Healthcare Patients Card Report
-    Odoo Healthcare Patients Medication History Report
-    Odoo Healthcare Appointments
-    Odoo hospital Appointments Invoice
-    Odoo Healthcare Families Prescriptions Healthcare Prescriptions
-    Odoo Healthcare Create Invoice from Prescriptions odoo hospital Prescription Report
-    Odoo Healthcare Patient Hospitalization
-    odoo Hospital Management System
-    Odoo Healthcare Management System
-    Odoo Clinic Management System
-    Odoo Appointment Management System
-    health care management system
-    Generate Report for patient details, appointment, prescriptions, lab-test
-
-    Odoo Lab Test Request and Result
-    Odoo Patient Hospitalization details
-    Generate Patient's Prescriptions
-
+    Módulo para la gestión clínica desarrollado por Innovoo, S.L.
+    Con NovooClinic se puede controlar la gestión de pacientes, citas, 
+    recetas, medicación, hospitalización, pruebas de laboratorio, etc.
     
 """,
 
@@ -114,7 +95,7 @@
     "installable": True,
     "application": True,
     "auto_install": False,
-    "images": ["static/description/Banner.gif"],
+    #"images": ["static/description/Banner.gif"],
     "live_test_url": 'https://youtu.be/fk9dY53I9ow',
     "license":'OPL-1',
 
